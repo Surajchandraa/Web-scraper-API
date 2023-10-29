93dc2048a289f472f0e91bd58f3a1ea4b4f97ad3
suraj chandra <120117179+Surajchandraa@users.noreply.github.com>
2023-10-29 08:57:01 +0530

    Update web_scrap.js

--- src/web_scrap.js
+++ src/web_scrap.js
@@ -88,11 +88,80 @@ function keywords(url,res){
         }
     })
 }
+
+function social_media(url,res){
+    request(url,(error,response,body)=>{
+        if(!error && response.statusCode==200){
+        const $ = cheerio.load(body);
+        let socialmedia={};
+        $('a').each((index,element)=>{
+            const href = $(element).attr('href');
+            if(href){
+                if(href.includes('facebook.com')){
+                    socialmedia.facebook=href;
+                }
+                else if(href.includes('instagram.com')){
+                    socialmedia.instagram=href;
+                }
+                else if(href.includes('linkedin.com')){
+                    socialmedia.linkedin=href;
+                }
+                else if(href.includes('twitter.com')){
+                    socialmedia.twitter=href;
+                }
+                else if(href.includes('github.com')){
+                    socialmedia.github=href;
+                }
+                else if(href.includes('youtube.com')){
+                    socialmedia.youtube=href;
+                }
+                else if(href.includes('pinterest.com')){
+                    socialmedia.pinterest=href;
+                }
+                else if(href.includes('tiktok.com')){
+                    socialmedia.tiktok=href;
+                }
+                else if(href.includes('reddit.com')){
+                    socialmedia.reddit=href;
+                }
+                else if(href.includes('discord.com')){
+                    socialmedia.discord=href;
+                }
+                else if(href.includes('skype.com')){
+                    socialmedia.skype=href;
+                }
+                else if(href.includes('whatsapp.com')){
+                    socialmedia.whatsapp=href;
+                }
+                else if(href.includes('tumblr.com')){
+                    socialmedia.tumblr=href;
+                }
+                else if(href.includes('quora.com')){
+                    socialmedia.quora=href;
+                }
+                
+            }
+        })
+        if (Object.keys(socialmedia).length === 0) {
+            socialmedia.not_found = "no links found";
+        }
+
+        res.send(socialmedia);
+
+        }
+        else{
+            res.send("error")
+        }
+    })
+}
+
+
 module.exports={
     paragraph,
     title,
     links,
     images,
     description,
-    keywords
+    keywords,
+    social_media
 }
